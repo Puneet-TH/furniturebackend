69b72ae86a10c61ea77bdeb43d4309c828205e37
Puneet-TH <22BCS13944@cuchd.in>
2025-08-29 00:24:15 +0530

    corrected get enquirie

--- Routes/enquiry.routes.js
+++ Routes/enquiry.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { getEnquiries, makeEnquiry } from '../Controller/Enquiry.controller.js';
+import { getUserEnquirie, makeEnquiry } from '../Controller/Enquiry.controller.js';
 import isAuthenticated from '../Middleware/isAuthenticated.js';
 const router = express.Router();
 
 
 
 router.route("/createEnquiry").post(isAuthenticated,makeEnquiry);
-router.route("/getEnquiries/:userId").get(isAuthenticated,getEnquiries);
+router.route("/getEnquiries/:userId").get(isAuthenticated,getUserEnquirie);
 
 
 export default router;
